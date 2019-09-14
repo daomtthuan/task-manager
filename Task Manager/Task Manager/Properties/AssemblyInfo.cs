b593983fdb24f5bc5819281013287d38411deb56
Dao Minh Trung Thuan <dao.mt.thuan@gmail.com>
2019-09-15 01:57:39 +0700

    up

---
+++ Task Manager/Task Manager/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Quản lý lịch trình")]
+[assembly: AssemblyDescription("Phần mềm quản lý lịch trình công việc")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+ [assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Dao Minh Trung Thuan")]
+[assembly: AssemblyProduct("Task Manager")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("Dao Minh Trung Thuan")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(true)]
+[assembly: Guid("9006f149-aa49-4b8e-ba69-386d945fa738")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("vi-VN")]
+
